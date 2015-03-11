f5651ee7222985179bbed79885cdcbf80ef34cc2
maximebf <maxime.bouroumeau@gmail.com>
2015-03-11 18:23:44 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco-babel',
-    version='0.2',
+    version='0.2.1',
     url='http://github.com/frascoweb/frasco-babel',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
